3aee4815407e1aff0021f85234b7a0573e0f9c4c
hptg1994 <hptg1994@gmail.com>
2019-01-05 16:55:13 -0600

    有条不紊的进行着

--- app/View/MainPage/main.js
+++ app/View/MainPage/main.js
@@ -1,5 +1,18 @@
 'use strict'
 
-angular.module("HomeModule", []).component('home', {
+/*angular.module("HomeModule", []).component('home', {
   templateUrl: 'View/MainPage/main.template.html'
-}).controller();
+}).controller('HomeController', ['$scope', 'Testing', function ($scope, Testing) {
+  Testing.test();
+}]);*/
+angular.module("HomeModule", []).directive('home', function () {
+  return {
+    restrict: "EA",
+    compile: function () {
+      console.log("it is call");
+    },
+    templateUrl: 'View/MainPage/main.template.html'
+  }
+}).controller('HomeController', ['$scope', 'Testing', function ($scope, Testing) {
+  Testing.test();
+}]);
--- app/app.service.js
+++ app/app.service.js
@@ -1,12 +1,26 @@
-'use strice'
+'use strict'
 
-angular.module("Global Services", []).service("CheckIfUserHasLogin", ['$http', function ($http) {
-  $http.get("localhost/Knowledge_PHP_Server/api/users/checkIfUserLogin.php").then((result) => {
-      if (result.data["Success"]) {
-        return true;
-      } else {
-        return false;
+angular.module("Global Services", [])
+  .factory("Testing", [function () {
+    return {
+      test: function () {
+        console.log("It is working")
       }
     }
-  );
-}]);
+
+  }])
+  .service("CheckIfUserHasLogin", ['$http', function ($http) {
+    $http.get("localhost/Knowledge_PHP_Server/api/users/checkIfUserLogin.php").then((result) => {
+        if (result.data["Success"]) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    );
+  }])
+  .service("GetMainPageQuestionList", ['$http', function ($http) {
+    $http.get('localhost/Knowledge_PHP_Server/api/question/getAllQuestion.php').then((result) => {
+      return (result.data);
+    })
+  }]);
